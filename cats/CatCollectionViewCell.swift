c8714e51ff8b331118278c753d5ca47d2986ae8f
Michelle Joudrey <michelle-joudrey@users.noreply.github.com>
2015-08-05 12:04:49 -0400

    Change main view controller to show cats

---
+++ cats/CatCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CatCollectionViewCell.swift
+//  cats
+//
+//  Created by Michelle J on 8/5/15.
+//  Copyright © 2015 mjoudrey. All rights reserved.
+//
+
+import UIKit
+
+class CatCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var catImageView : UIImageView?
+}
---
+++ cats/CatsViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  cats
+//
+//  Created by Michelle J on 8/5/15.
+//  Copyright © 2015 mjoudrey. All rights reserved.
+//
+
+import UIKit
+
+class CatsViewController : UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 200
+    }
+    
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("CatCollectionViewCell", forIndexPath: indexPath) as! CatCollectionViewCell
+        cell.backgroundColor = UIColor.blackColor()
+        cell.catImageView?.image = UIImage(named: "286.jpg")
+        return cell
+    }
+
+
+}
+
